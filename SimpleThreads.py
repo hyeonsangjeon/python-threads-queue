291e1487ff29fbebfe2fdd054cccc2b7f46ea337
hyeonsangjeon <wingnut0310@gmail.com>
2018-09-18 18:15:13 +0900

    mod

--- SimpleThreads.py
+++ SimpleThreads.py
@@ -12,17 +12,17 @@ def consumer():
         # ~~~
 
         exqueue.task_done()  # If task_done(), producer does not have to queue or poll consuming threads with joins.
-        print('[Consumer] Processing Done')
+        print("[Consumer] Processing Done")
 
 thread = Thread(target=consumer).start()
 
 exqueue.put(object())
-print('[Producer] Put into Queue 1st')
+print("[Producer] Put into Queue 1st")
 exqueue.put(object())
-print('[Producer] Put into Queue 2nd')
+print("[Producer] Put into Queue 2nd")
 
 exqueue.join()
-print('[Producer] stand by')
+print("[Producer] stand by")
 #As is obvious, if the queue is empty, the join method of queue does not complete until all tasks have been added to the queue call task_done.
 
 
